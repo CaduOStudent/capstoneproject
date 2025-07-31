f3f91a0150977745e2bb7ebe3c4134983f6fb596
Cadu Menezes <2023252@student.cct.ie>
2025-07-31 15:15:21 +0100

    feat: Creating model for book, next step, serializer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# The models are the "class" in python
+# They are used to create the database tables
+# and define the structure of the data
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set on creation
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
