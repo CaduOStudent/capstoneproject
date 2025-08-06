86ce2abd4d60948af98b316288bee7fd89525e6f
Cadu Menezes <2023252@student.cct.ie>
2025-08-06 09:33:56 +0100

    feat: adding feat of bulk creation

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import health_view, book_view, book_detail_view
+from .views import health_view, book_view, book_detail_view, bulk_create_books_view
 
 urlpatterns = [
     path('health/', health_view, name='health'),
     path('books/', book_view, name='book-list'),
     path('books/<int:pk>/', book_detail_view, name='book-detail'),
     # Add any additional paths here
-    # Example: path('books/bulk-create/', BulkCreateBooksView.as_view(), name='bulk-create-books'),
+    path('books/bulk-create/', bulk_create_books_view, name='bulk-create-books'),
 ]
